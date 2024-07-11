d239f053483e0d142634efad60ddae2e0a5e2626
Luiz_Paulo11 <Lpsantosvieira@gmail.com>
2024-07-11 20:02:55 -0300

    Coneção ao MySQL

---
+++ src/connection.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados:', err.stack);
+        return;
+    }
+    console.log('Conectado ao banco de dados MySQL com ID de conexão ' + connection.threadId);
+});
+
+module.exports = connection;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const connection = require('./connection');
 
-const port = 3000;
+dotenv.config();
+
+const port = process.env.PORT || 3000;
 const app = express();
 
-
-
-
-
-
+app.get('/', (req, res) => {
+    res.send('Olá Mundo!');
+});
 
 
 app.listen(port, () => {
-    console.log('O servidor esta funcionando corretamente')
+    console.log(`Servidor iniciado na porta ${port}`);
+    
+    connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+        if (error) {
+            console.error('Erro ao conectar ao banco de dados:', error);
+        } else {
+            console.log('Conexão com o MySQL estabelecida com sucesso!');
+        }
+    });
 });
