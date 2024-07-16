149a5227b4bef0058cd06b171963417b9090ca22
Luiz_Paulo11 <Lpsantosvieira@gmail.com>
2024-07-16 20:19:22 -0300

    Criação de todas as rotas

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const connection = require('./connection');  // Importa a conexão com o banco de dados
+const connection = require('./connection');
 const app = express();
 
-// Carrega as variáveis de ambiente do arquivo .env
+
 dotenv.config();
 
-// Importa o roteador de usuários
+
 let UserRouter = require('./UserRouter');
 
-// Usa o roteador para todas as rotas que começam com '/'
-app.use('/', UserRouter);
 
-// Define a porta do servidor
+app.use('/api', UserRouter);
+
+
 const port = process.env.PORT || 3000;
 
 // Inicia o servidor
 app.listen(port, () => {
     console.log(`Servidor iniciado na porta ${port}`);
     
-    // Verifica a conexão com o banco de dados
+
     connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
         if (error) {
             console.error('Erro ao conectar ao banco de dados:', error);
